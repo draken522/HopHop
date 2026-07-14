0a5ad371c1fc756b6dacff93aeca2a8581b335e8
draken522 <b954869@gmail.com>
2026-07-14 13:33:26 +0330

    keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hop Bot is running!"
+
+
+def run():
+    app.run(host="0.0.0.0", port=10000)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
